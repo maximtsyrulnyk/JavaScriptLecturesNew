96b6a01564cca583df380e6e3660a4957ba1bbfd
maximtsyrulnyk <126355452+maximtsyrulnyk@users.noreply.github.com>
2024-01-31 13:56:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+// let name, age;
+// // let age;
+
+// age = 10;
+// age = 15;
+// age = 20;
+// age = 30; age = 40; age = 50;
+// age = name = 'Ivan';
+// // name = "Maksym";
+// // const price = 100;
+// var color = "#fff";
+// var color = "#000";
+
+// console.log('Log:', age, name, color);
+
+// let n = -99999999999999999999999999999999n;
+// let b = NaN ** 1;
+// let c = -Infinity;
+// console.log(n, b);
+// console.log(-1/0);
+// console.log(99999999999999999999999999999999 > Infinity);
+// const t = 'hello';
+// const p = '"hello"';
+// const l = `hello ${t} ${2+2}`;
+
+// console.log(l);
+// let yes = true;
+// let no = false;
+
+// let isAdmit = false;
+// let none = null;
+
+// console.log(typeof p);
+
+const a = 1 - 2 - 2 - 2 - 2 + 2;
+const b = `'Test Hello World`;
+const c = 1 - "10";
+const d = "10" ** 1;
+const e = 10 / "10";
+let f = "5" ** "4";
+let g = 6 % 7;
+// console.log(f, g);
+// f = g-- + 1;
+// console.log(f, g);
+// console.log(f);
+f = 5;
+f %= 2;
+console.log(f);
